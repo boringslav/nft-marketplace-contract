c9b4c84acddd82761a79bf3e22adc06b828c0447
Boringslav <bostoycontact@gmail.com>
2023-08-14 13:25:28 +0300

    NFT tests

--- test/TestNFT.t.sol
+++ test/TestNFT.t.sol
@@ -8,6 +8,7 @@ import {NFT} from "../src/NFT.sol";
 contract TestNFT is Test {
     string constant NFT_NAME = "TEST_NAME";
     string constant NFT_SYMBOL = "TEST_SYMBOL";
+    string constant NFT_URI = "TEST_URI";
     NFT public nft;
     DeployNFT public deployer;
 
@@ -19,5 +20,16 @@ contract TestNFT is Test {
     function testInitialization() external {
         assertEq(nft.name(), NFT_NAME);
         assertEq(nft.symbol(), NFT_SYMBOL);
+    }
+
+    function testMintShouldUpdateTokenUriMapping() external {
+        uint256 tokenId = nft.mint(NFT_URI);
+        assertEq(nft.tokenURI(tokenId), NFT_URI);
+    }
+
+    function testMintShouldUpdateTokenCounter() external {
+        uint256 tokenCounter = nft.getTokenCounter();
+        nft.mint(NFT_URI);
+        assertEq(nft.getTokenCounter(), tokenCounter + 1);
     }
 }
